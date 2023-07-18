38e3e60ea8bcd248fe0793ae1e4ce9205f86e1d8
ethan <hertlethan@gmail.com>
2023-07-17 21:18:46 -0400

    further work on 3-4

--- module 3/3-4/Unsolved/script.js
+++ module 3/3-4/Unsolved/script.js
@@ -1,9 +1,10 @@
 // Write Your JavaScript Code Here
 
-var personName = "Ethan";
+var name = "Ethan";
 var pets = 2;
 var funFact = "I CAN'T TAKE IT ANYMORE";
 
-console.log("My name is " + personName);
+console.log("My name is " + name);
 console.log("I have " + pets + " pets");
 console.log("Fun fact: " + funFact);
+console.log(funFact, name);
