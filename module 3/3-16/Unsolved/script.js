9359e5c183a5ffcb94820e3200314017d37f264d
ethan <hertlethan@gmail.com>
2023-07-19 21:32:44 -0400

    completed activity 3-16

--- module 3/3-16/Unsolved/script.js
+++ module 3/3-16/Unsolved/script.js
@@ -1,10 +1,16 @@
 // isEqual using function declaration
-function isEqual(x, y) {
-  if (x === y) {
+function isEqual(x, y) 
+{
+  if (x === y) 
+  {
     console.log('They are equal in type and value');
-  } else if (x == y) {
+  } 
+  else if (x == y) 
+  {
     console.log('They are equal in value');
-  } else {
+  } 
+  else 
+  {
     console.log('They are not equal');
   }
   return;
@@ -16,7 +22,25 @@ isEqual(10, 10);
 // Refer to sample functions as needed!
 
 // TODO: Call the isEqual function so that it logs "They are equal in value"
+isEqual(10, "10");
 
 // TODO: Rewrite isEqual as a function expression called 'isEqualTakeTwo`
+function isEqualTakeTwo(x, y)
+{
+  if (x === y)
+  {
+    console.log('They are equal in type and value');
+  }
+  else if (x == y)
+  {
+    console.log('They are equal in value');
+  }
+  else 
+  {
+    console.log('They are not equal');
+  }
+  return;
+}
 
 // TODO: Call the isEqualTakeTwo function so that it logs "They are not equal"
+isEqualTakeTwo(10, "hi there");
