56934fb96c97b915e9efa9c23b1a69d2543de1ca
ethan <hertlethan@gmail.com>
2023-07-19 21:23:28 -0400

    completed activity 3-12 & 3-14

--- module 3/3-12/Unsolved/script.js
+++ module 3/3-12/Unsolved/script.js
@@ -1,1 +1,10 @@
 // WRITE YOUR CODE HERE
+
+var studentNames = ["Ethan", "Milena", "Regina", "Junior"];
+
+console.log(studentNames.length);
+
+console.log("Welcome to the class", studentNames[0])
+console.log("Welcome to the class", studentNames[1])
+console.log("Welcome to the class", studentNames[2])
+console.log("Welcome to the class", studentNames[3])
