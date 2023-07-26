10e2f5409c462989b96b7cfb7378eb2c67e7c321
ethan <hertlethan@gmail.com>
2023-07-26 18:35:29 -0400

    idk

--- module 4/4-4-Stu_Traverse-DOM/Unsolved/assets/js/script.js
+++ module 4/4-4-Stu_Traverse-DOM/Unsolved/assets/js/script.js
@@ -1,8 +1,9 @@
 // Access element using id
 var articlesDiv = document.getElementById("articles");
-var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
 articlesDiv.style.fontSize = '50px';
 headerDiv.children[0].style.color = 'white';
+
+var headerDiv = document.getElementById('header');
 headerDiv.children[1].style.color = 'white';
