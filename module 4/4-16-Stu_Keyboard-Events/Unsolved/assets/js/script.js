a2443009fb61742e6cdab174c200800131793dbb
ethan <hertlethan@gmail.com>
2023-07-27 19:05:35 -0400

    completed activity 4-16

--- module 4/4-16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ module 4/4-16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,5 +1,9 @@
 function keydownAction(event) {
-  // TODO: Complete keydown function
+  document.querySelector("#status").innerHTML = "KEYDOWN Event";
+  var keyPressed = event.key;
+  var keyCode = event.keyCode;
+  document.querySelector("#key").innerHTML = keyPressed;
+  document.querySelector("#code").innerHTML = keyCode;
 }
 
 function keyupAction() {
@@ -7,5 +11,6 @@ function keyupAction() {
 }
 
 document.addEventListener("keyup", keyupAction);
+document.addEventListener("keydown", keydownAction);
 // TODO: Add Event Listener for "keydown" event
 
