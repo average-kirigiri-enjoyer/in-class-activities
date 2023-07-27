e80271ecb9893e74bd43f27af0d627d088334d94
ethan <hertlethan@gmail.com>
2023-07-26 20:36:17 -0400

    completed activity 4-12

--- module 4/4-12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ module 4/4-12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -7,6 +7,28 @@ function setCounterText() {
   countEl.textContent = count;
 }
 
+function increment()
+{
+  count++;
+  setCounterText();
+}
+
+function decrement()
+{
+  count--;
+
+  if (count < 0)
+  {
+    count = 0;
+    return;
+  }
+  
+  setCounterText();
+}
+
 // TODO: Add event listener to increment button
+
+incrementEl.addEventListener("click", increment);
+decrementEl.addEventListener("click", decrement);
 
 // TODO: Add event listener to decrement button 
