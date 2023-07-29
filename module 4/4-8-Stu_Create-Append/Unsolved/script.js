4eae8a735bedd1d5738ab1154d44c70b60937250
ethan <hertlethan@gmail.com>
2023-07-29 10:38:06 -0400

    completed activity 4-8

--- module 4/4-8-Stu_Create-Append/Unsolved/script.js
+++ module 4/4-8-Stu_Create-Append/Unsolved/script.js
@@ -17,6 +17,10 @@ h1El.textContent = "Welcome to my page";
 kittenEl.textContent = "This is my kitten 🐱.";
 nameEl.textContent = "His name is Jax.";
 favoriteEl.textContent = "My favorite foods are:";
+li1.textContent = "cucumbers";
+li2.textContent = "doritos";
+li3.textContent = "blood of the innocent";
+li4.textContent = "cat food";
 
 body.appendChild(h1El);
 body.appendChild(infoEl);
@@ -33,6 +37,24 @@ infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
 imgEl.setAttribute("src", "http://placekitten.com/200/300");
 nameEl.setAttribute("style", "font-size:25px; text-align:center;");
 kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
-favoriteEl.setAttribute("style", "font-size:20px;");
+favoriteEl.setAttribute("style", "font-size:20px; padding: 20px; background-color: #333333");
 
 // TODO: Add ordered list items containing four favorite foods
+listEl.appendChild(li1)
+listEl.appendChild(li2)
+listEl.appendChild(li3)
+listEl.appendChild(li4)
+
+/* THIS IS POINTLESS WITH THE STUFF THAT COMES AFTER, BUT IS GOOD FOR REFERENCING SYNTAX
+for (child = 0; child < 4; child++)
+{
+    listEl.children[child].setAttribute("style", "color: white; padding: 5px; margin-left: 35px");
+}
+*/
+
+listEl.children[0].setAttribute("style", "background-color: green; color: white; padding: 5px; margin-left: 35px");
+listEl.children[1].setAttribute("style", "background-color: red; color: white; padding: 5px; margin-left: 35px");
+listEl.children[2].setAttribute("style", "background-color: blue; color: white; padding: 5px; margin-left: 35px");
+listEl.children[3].setAttribute("style", "background-color: purple; color: white; padding: 5px; margin-left: 35px");
+
+
