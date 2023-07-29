ee5b41b3d7aff59ecc4b364fe50df5d0498a254e
ethan <hertlethan@gmail.com>
2023-07-29 11:37:49 -0400

    completed activity 4-24 & 4-26

--- module 4/4-24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ module 4/4-24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -8,7 +8,15 @@ signUpButton.addEventListener("click", function(event) {
   event.preventDefault();
   
   // TODO: Create user object from submissio
+  var signUpData =
+  {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value
+  };
 
   // TODO: Set new submission to local storage 
+  localStorage.setItem("signUpData", JSON.stringify(signUpData));
   
 });
--- module 4/4-26-Stu_Local-Storage-Todos/Unsolved/assets/js/script.js
+++ module 4/4-26-Stu_Local-Storage-Todos/Unsolved/assets/js/script.js
@@ -5,13 +5,13 @@ var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = [];
 
-// TODO: What is the purpose of this function?
+//processes and displays information regards todo items
 function renderTodos() {
-  // TODO: Describe the functionality of the following two lines of code.
+  //resets inner HTML of todo list, and sets the todo count to the number of items in the array containing todo items
   todoList.innerHTML = "";
   todoCountSpan.textContent = todos.length;
   
-  // TODO: Describe the functionality of the following `for` loop.
+  //creates and displays a list item for each todo item
   for (var i = 0; i < todos.length; i++) {
     var todo = todos[i];
 
@@ -27,47 +27,47 @@ function renderTodos() {
   }
 }
 
-// TODO: What is the purpose of the following function?
+//load and render any already-existing todo items from local storage
 function init() {
-  // TODO: What is the purpose of the following line of code?
+  //retrieve any already-existing todo items from local storage, converting them back from a JSON string to their original form
   var storedTodos = JSON.parse(localStorage.getItem("todos"));
-  // TODO: Describe the functionality of the following `if` statement.
+  //if there are todo items in local storage, set the todo list to the local storage content
   if (storedTodos !== null) {
     todos = storedTodos;
   }
-  // TODO: Describe the purpose of the following line of code.
+  //updates todo list now that local storage data has been retrieved
   renderTodos();
 }
 
 function storeTodos() {
-  // TODO: Describe the purpose of the following line of code.
+  //stores todo list data as a JSON string in local storage
   localStorage.setItem("todos", JSON.stringify(todos));
 }
-// TODO: Describe the purpose of the following line of code.
+//code runs when the user submits a todo item
 todoForm.addEventListener("submit", function(event) {
   event.preventDefault();
   var todoText = todoInput.value.trim();
-  // TODO: Describe the functionality of the following `if` statement.
+  //exits function if the user did not input anything to submit
   if (todoText === "") {
     return;
   }
- // TODO: Describe the purpose of the following lines of code.
+  //adds todo item to todo list array, and resets input box
   todos.push(todoText);
   todoInput.value = "";
  
-  // TODO: What will happen when the following functions are called?
+  //updated todo list data is sent to local storage, and new todo list item is visually rendered to the page
   storeTodos();
   renderTodos();
 });
 
-// TODO: Describe the purpose of the following line of code.
+//code runs when the user clicks on the todo list box
 todoList.addEventListener("click", function(event) {
   var element = event.target;
-  // TODO: Describe the functionality of the following `if` statement.
+  //checks if the element the user clicked on was the "complete" button for a todo item; if so, remove that item from the list
   if (element.matches("button") === true) {
     var index = element.parentElement.getAttribute("data-index");
     todos.splice(index, 1);
-    // TODO: What will happen when the following functions are called?
+    //updated todo list data is sent to local storage, and new todo list item is visually rendered to the page
     storeTodos();
     renderTodos();
   }
