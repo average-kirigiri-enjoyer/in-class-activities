38b01845df484a18be6683c507cf71145b87cf91
ethan <hertlethan@gmail.com>
2023-08-02 20:22:55 -0400

    completed activities 5-6 & 5-8

--- module 5/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ module 5/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,26 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
+var shoppingInput = $('input[name="shopping-input"]');
+var shoppingButton = $(".btn");
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+
+//prevent page refresh
+
+function addListItem(event)
+{
+    event.preventDefault();
+    var newListItem = $("<li>");
+
+    console.log(shoppingInput.val())
+    newListItem.text(shoppingInput.val());
+    shoppingListEl.append(newListItem);
+    shoppingInput.val(""); 
+}
+
+shoppingButton.on("click", addListItem);
+
+//take input text and add as list element
+//clear input
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
--- module 5/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ module 5/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -3,4 +3,8 @@ var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
 
+rootEl.children("ul").children("li").css({"background-color": "#FFFFFF"});
+
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+
+rootEl.children("ul").children(".item-a3").text("O");
