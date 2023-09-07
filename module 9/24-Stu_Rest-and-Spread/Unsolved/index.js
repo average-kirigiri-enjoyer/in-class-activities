641c4ded87327419f61f9704398987f3453f09b0
ethan <hertlethan@gmail.com>
2023-09-06 21:14:10 -0400

    completed activitys 9-22 & 9-24

--- module 9/24-Stu_Rest-and-Spread/Unsolved/index.js
+++ module 9/24-Stu_Rest-and-Spread/Unsolved/index.js
@@ -2,27 +2,34 @@
 const songs = ['Creep', 'Everlong', 'Bulls On Parade', 'Song 2', 'What I Got'];
 
 // TODO: Which operator is being used here?
+// rest operator
 const newSongs = [...songs];
 
 // TODO: What do you expect to be logged in the console?
+// each item in the array 'songs'
 console.log(newSongs);
 
 // Exercise 2
 const addition = (x, y, z) => {
   const array = [x, y, z];
   // TODO: What does the reduce() method do?
+  // performs a user-defined function on the array
   return array.reduce((a, b) => a + b, 0);
 };
 // TODO: What do you expect to be logged in the console?
+// 6
 console.log(addition(1, 2, 3));
 
 // TODO: What is this syntax that is being used as the parameter?
+// rest parameter
 const additionSpread = (...array) => {
   return array.reduce((a, b) => a + b, 0);
 };
 
 // TODO: What do you expect to be logged in the console?
+// 6
 console.log(additionSpread(1, 2, 3));
 
 // TODO: What do you expect to be logged in the console?
+// 110
 console.log(additionSpread(1, 2, 3, 4, 100));
