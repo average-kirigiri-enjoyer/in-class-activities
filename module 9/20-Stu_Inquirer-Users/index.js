a7f042a1f8d7f2be57c46ebde06965aec2c8b48d
ethan <hertlethan@gmail.com>
2023-09-06 21:13:31 -0400

    completed activity 9-20

--- module 9/20-Stu_Inquirer-Users/index.js
+++ module 9/20-Stu_Inquirer-Users/index.js
@@ -1,28 +1,42 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-let content = "";
-
 inquirer
-    .prompt([
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "What languages do you know?",
-        name: "languages",
-    },
-    {
-        type: "input",
-        message: "What is your preferred method of communcation?",
-        name: "preferredCommuncation",
-    },
-    ])
-    .then((response) =>
-        content = response,
-        fs.writeFile("./nameAndLanguages.txt", content)
-    );
+	.prompt([
+	{
+		type: "input",
+		message: "What is your name?",
+		name: "name",
+	},
+	{
+		type: "checkbox",
+		message: "What languages do you know?",
+		name: "languages",
+		choices: ["HTML", "CSS", "JavaScript", "Arduino", "Python"],
+	},
+	{
+		type: "list",
+		message: "What is your preferred method of communcation?",
+		name: "communcation",
+		choices: ["Speech", "Hand signals", "Telepathy"],
+	},
+	])
+	.then(function(data)
+	{
+		const name = `Name: ${data.name}`;
+		const languages = `Languages: ${data.languages}`;
+		const communcation = `Communcation Method: ${data.communcation}`;
+		const userInputData = name + "\n" + languages + "\n" + communcation;
 
+		fs.writeFile("./name.txt", userInputData, function(err)
+		{
+			if (err)
+			{
+				console.log(err);
+			}
+			else
+			{
+				console.log("data saved to name.txt");
+			}
+		});
+	});
