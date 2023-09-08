04f53a69dd7a15d14a5d80c5388d96c7f603b59a
ethan <hertlethan@gmail.com>
2023-09-07 21:41:39 -0400

    completed activity 9-08

--- module 10/08-Stu_Inheritance/Unsolved/index.js
+++ module 10/08-Stu_Inheritance/Unsolved/index.js
@@ -1,5 +1,5 @@
 // TODO: Create a class constructor named ForumItem that takes in 'authorName', 'text', and 'createdOn'.
-
+// ES6
 class ForumItem
 {
   constructor(authorName, text, createdOn)
@@ -10,25 +10,57 @@ class ForumItem
   }
 }
 
-// TODO: Setup BlogPost and Comment so they inherit their structure from ForumItem.
-class BlogPost extends ForumItem {
-  constructor(authorName, title, text, createdOn) {
-    super(this, authorName, text, createdOn);
+class BlogPost extends ForumItem
+{
+  constructor(authorName, title, text, createdOn)
+  {
+    super(authorName, text, createdOn);
     this.title = title;
     this.comments = [];
   }
 
-  addComment(comment) {
+  addComment(comment)
+  {
     this.comments.push(comment);
   }
 }
 
-class Comment extends ForumItem {
-  constructor(authorName, text, createdOn, reaction) {
-    super(this, authorName, text, createdOn);
+class Comment extends ForumItem
+{
+  constructor(authorName, text, createdOn, reaction)
+  {
+    super(authorName, text, createdOn);
     this.reaction = reaction;
   }
 }
+
+/* ES5
+function ForumItem(authorName, text, createdOn)
+{
+  this.authorName = authorName;
+  this.text = text;
+  this.createdOn = createdOn;
+}
+
+function BlogPost(authorName, title, text, createdOn)
+{
+  ForumItem.call(this, authorName, text, createdOn);
+  this.title = title;
+  this.comments = [];
+}
+
+BlogPost.prototype.addComment = function(comment)
+{
+  this.comments.push(comment);
+}
+
+function Comment(authorName, text, createdOn, reaction)
+{
+  ForumItem.call(this, authorName, text, createdOn);
+  this.reaction = reaction;
+}*/
+
+// TODO: Setup BlogPost and Comment so they inherit their structure from ForumItem.
 
 // TODO: Create a new object using the Comment class constructor.
 
