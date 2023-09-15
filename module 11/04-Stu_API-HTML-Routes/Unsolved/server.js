06d1a6c70b4616a4d24384b0fcd0b710519101de
ethan <hertlethan@gmail.com>
2023-09-14 20:30:33 -0400

    completed activity 11-2, started activity 11-4

--- module 11/04-Stu_API-HTML-Routes/Unsolved/server.js
+++ module 11/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,11 +1,23 @@
 // TODO: Import express
+const express = require("express");
+const fs = require("fs");
 
 // TODO: Import 'terms.json' file
 const PORT = 3001;
 
 // TODO: Initialize app variable
 
+const app = express();
+
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
+
+app.get("/api", (req, res) =>
+{
+  res.json({
+    term: "api",
+    description: "description of web development terms"
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
