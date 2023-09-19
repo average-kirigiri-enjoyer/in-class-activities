8b86da120dc99bc564d09b9409bab2a1e65e4949
ethan <hertlethan@gmail.com>
2023-09-18 21:27:32 -0400

    completed activities 11-8 -> 11-16

--- module 11/14-Stu_POST-Requests/Unsolved/server.js
+++ module 11/14-Stu_POST-Requests/Unsolved/server.js
@@ -7,17 +7,27 @@ const app = express();
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
 app.get('/api/reviews', (req, res) => {
   // Your code here
+    console.log("REVIEW GET REQUEST RECEIVED!!!");
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
+app.post('/api/reviews', (req, res) =>
+{
+  console.log("REVIEW POST REQUEST RECEIVED!!!");
+});
 // Your code here
 
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
 app.get('/api/upvotes', (req, res) => {
   // Your code here
+  console.log("UPVOTE GET REQUEST RECEIVED!!!");
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
+app.post('/api/upvotes', (req, res) => {
+  // Your code here
+  console.log("UPVOTE POST REQUEST RECEIVED!!!");
+});
 // Your code here
 
 app.listen(PORT, () =>
