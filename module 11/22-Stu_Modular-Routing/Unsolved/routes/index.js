853caf779ded77dff559b948ddd5751b1e8d6b82
ethan <hertlethan@gmail.com>
2023-09-21 14:11:02 -0400

    activity 11-22

--- module 11/22-Stu_Modular-Routing/Unsolved/routes/index.js
+++ module 11/22-Stu_Modular-Routing/Unsolved/routes/index.js
@@ -1,9 +1,16 @@
 // TODO: Import express
+const express = require("express");
 
 // TODO: Import modules for tips/feedback
+const feedback = require("./feedback");
+const tips = require("./tips");
 
 // TODO: Create app variable to create an instance of express()
+const app = express();
 
 // TODO: Use our routes
+app.use("/feedback", feedback);
+app.use("/tips", tips);
 
 // TODO: Export app
+module.exports = app;
