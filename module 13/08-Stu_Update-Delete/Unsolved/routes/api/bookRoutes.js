fe9744d9b881e93f885461bd568c0558fd0a7e85
ethan <hertlethan@gmail.com>
2023-10-02 20:18:00 -0400

    completed activities 13-8 & 13-10

--- module 13/08-Stu_Update-Delete/Unsolved/routes/api/bookRoutes.js
+++ module 13/08-Stu_Update-Delete/Unsolved/routes/api/bookRoutes.js
@@ -1,15 +1,66 @@
 const router = require('express').Router();
 const Book = require('../../models/Book');
 
+// GET all books
+router.get('/', (req, res) => {
+  // Get all books from the book table
+  Book.findAll().then((bookData) => {
+    res.json(bookData);
+  });
+});
+// GET a book
+router.get('/:isbn', (req, res) => {
+  // Get one book from the book table
+  Book.findOne(
+    {
+      // Gets the book based on the isbn given in the request parameters
+      where: {
+        isbn: req.params.isbn
+      },
+    }
+  ).then((bookData) => {
+    res.json(bookData);
+  });
+});
+
 // TODO finish the PUT route to UPDATE a book in the database with a matching book_id
 router.put('/:book_id', (req, res) => {
-  
+  Book.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      title: req.body.title,
+      author: req.body.author,
+      isbn: req.body.isbn,
+      pages: req.body.pages,
+      edition: req.body.edition,
+      is_paperback: req.body.is_paperback,
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        book_id: req.params.book_id,
+      },
+    }
+  )
+    .then((updatedBook) => {
+      // Sends the updated book as a json response
+      res.json(updatedBook);
+    })
+    .catch((err) => res.json(err));
   
 });
 
 // TODO finish the DELETE route to DELETE a book in the database with a matching book_id
 router.delete('/:book_id', (req, res) => {
-  
+  Book.destroy({
+    where: {
+      book_id: req.params.book_id,
+    },
+  })
+    .then((deletedBook) => {
+      res.json(deletedBook);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.post('/seed', (req, res) => {
--- module 13/10-Stu_Async-Await/Unsolved/routes/api/bookRoutes.js
+++ module 13/10-Stu_Async-Await/Unsolved/routes/api/bookRoutes.js
@@ -1,47 +1,76 @@
 const router = require('express').Router();
 const Book = require('../../models/Book');
 
-// Updates book based on its book_id
-router.put('/:book_id', (req, res) => {
-  //Calls the update method on the Book model
-  Book.update(
+// GET all books
+router.get('/', (req, res) => {
+  // Get all books from the book table
+  Book.findAll().then((bookData) => {
+    res.json(bookData);
+  });
+});
+// GET a book
+router.get('/:isbn', (req, res) => {
+  // Get one book from the book table
+  Book.findOne(
     {
-      // All the fields you can update and the data attached to the request body.
-      title: req.body.title,
-      author: req.body.author,
-      isbn: req.body.isbn,
-      pages: req.body.pages,
-      edition: req.body.edition,
-      is_paperback: req.body.is_paperback,
-    },
-    {
-      // Gets a book based on the book_id given in the request parameters
+      // Gets the book based on the isbn given in the request parameters
       where: {
-        book_id: req.params.book_id,
+        isbn: req.params.isbn
       },
     }
-  )
-    .then((updatedBook) => {
-      res.json(updatedBook);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
+  ).then((bookData) => {
+    res.json(bookData);
+  });
+});
+
+// Updates book based on its book_id
+router.put('/:book_id', async (req, res) => {
+  //Calls the update method on the Book model
+  try
+  {
+    const updatedBook = Book.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        title: req.body.title,
+        author: req.body.author,
+        isbn: req.body.isbn,
+        pages: req.body.pages,
+        edition: req.body.edition,
+        is_paperback: req.body.is_paperback,
+      },
+      {
+        // Gets a book based on the book_id given in the request parameters
+        where: {
+          book_id: req.params.book_id,
+        },
+      }
+    )
+    
+    res.json(updatedBook);
+  }
+  catch(err)
+  {
+    console.log(err);
+    res.json(err);
+  }
 });
 
 // Delete route for a book with a matching book_id
-router.delete('/:book_id', (req, res) => {
+router.delete('/:book_id', async (req, res) => {
   // Looks for the books based book_id given in the request parameters
-  Book.destroy({
-    where: {
-      book_id: req.params.book_id,
-    },
-  })
-    .then((deletedBook) => {
-      res.json(deletedBook);
-    })
-    .catch((err) => res.json(err));
+  try
+  {
+    const deleteBook = await Book.destroy(
+    {
+      where: {book_id: req.params.book_id}
+    });
+
+    res.json(deletedBook);
+  }
+  catch(err)
+  {
+    res.json(err);
+  }
 });
 
 module.exports = router;
